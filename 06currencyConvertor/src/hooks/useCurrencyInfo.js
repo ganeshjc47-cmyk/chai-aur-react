8f642fefef45daa0f99becfdaf95b74944e79a63
Ganesh <ganeshjc47@gmail.com>
2026-02-28 14:26:52 +0530

    fix: unused useEffect dependency data

--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -1,16 +1,28 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
 
-function useCurrencyInfo(currency){
-    const [data, setData] = useState({})
-     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
-        .then((res) => res.json())
-        .then((res) => setData(res[currency]))
-        console.log(data);      
-     },[currency])
-     console.log(data);
-     return data
+  useEffect(() => {
+    const fetchCurrencyData = async () => {
+      try {
+        const response = await fetch(
+          `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+        );
+
+        const result = await response.json();
+        setData(result[currency]);
+      } catch (error) {
+        console.error("Error fetching currency data:", error);
+      }
+    };
+
+    if (currency) {
+      fetchCurrencyData();
+    }
+  }, [currency]);
+
+  return data;
 }
 
 export default useCurrencyInfo;
