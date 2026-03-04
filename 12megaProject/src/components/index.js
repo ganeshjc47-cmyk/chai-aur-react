cb0666acc836f33169ba65030a0921c9772b4e0e
Ganesh <ganeshjc47@gmail.com>
2026-03-04 23:48:47 +0530

    feat: add footer.jsx and button.jsx

--- 12megaProject/src/components/index.js
+++ 12megaProject/src/components/index.js
@@ -1,7 +1,10 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-
+import container from "./container/Container"
+import Logo from "./Logo"
 export {
     Header,
-    Footer
+    Footer,
+    container,
+    Logo
 }
