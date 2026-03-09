b5f20dc2304ab85a3560d0a3263f7f5602edcf6d
Ganesh <ganeshjc47@gmail.com>
2026-03-09 23:45:27 +0530

    feat: implement core blog architecture wit Appwrite integration and UI components

--- 12megaProject/src/components/index.js
+++ 12megaProject/src/components/index.js
@@ -2,9 +2,25 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import container from "./container/Container"
 import Logo from "./Logo"
+import LogoutBtn from './Header/LogoutBtn'
+import RTE from "./RTE";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Post from "./Post"
+import PostForm from "./post-form/PostForm"; 
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
 export {
     Header,
     Footer,
     container,
-    Logo
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Post,
+    PostForm,
+    PostCard,
+    AuthLayout
 }
